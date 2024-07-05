1724015cef2338892682f1653aeaf603b90fc052
Saim Khalid <saim.khalid@ucalgary.ca>
2024-07-04 18:16:40 -0600

    Changed message to include my first name

---
+++ ENSF380LAB1/src/srf/Application.java
@@ -0,0 +1,13 @@
+package srf;
+
+import java.lang.runtime.ObjectMethods;
+
+public class Application extends ObjectMethods {
+
+	public static void main(String[] args) {
+		System . out . println (" Hello , Saim !") ;
+		// TODO Auto-generated method stub
+
+	}
+
+}
